7282803ec0dd5c0d0f0327d7ee9e7a8bb642b27a
Anthony Pilloud <a.pilloud@onlineformapro.com>
2017-12-04 13:13:01 +0100

    [func] adding base of flash message

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const extractSass = new ExtractTextPlugin({
+    filename: "./dist/css/style.css",
+    disable: process.env.NODE_ENV === "development"
+});
+
+module.exports = {
+    entry: ['./source/scss/style.scss', './source/js/ribs-flash-message.js'],
+    output: {
+        filename: 'dist/js/ribs-flash-message.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                include: path.join(__dirname, 'assets/'),
+                loader: 'babel-loader',
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: extractSass.extract({
+                    use: [
+                        {loader: 'css-loader'},
+                        {loader: 'sass-loader'}
+                    ],
+                    fallback: "style-loader"
+                })
+            }
+        ]
+    },
+    plugins: [
+        new UglifyJsPlugin(),
+        extractSass
+    ]
+};
