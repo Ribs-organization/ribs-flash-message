74db5df39895b7d8d3ef5e177d66d583e7e18e18
Anthony Pilloud <pilloud.anthony@gmail.com>
2017-12-16 13:19:09 +0100

    [func] add method to set position top of element to move it down when multiple flash are displayed

--- source/js/ribs-flash-message.js
+++ source/js/ribs-flash-message.js
@@ -2,11 +2,31 @@ import $ from 'jQuery';
 
 class RibsFlashMessage {
 	constructor() {
+		this.flash = $('.RibsFlashMessage');
+		this.setFlashPosition();
 		this.displayFlash();
 	}
 
+	setFlashPosition() {
+		const initialTop = 40;
+		const nextTop = initialTop / 2;
+		let topPos = this.flash.position().top;
+
+		this.flash.each((index, element) => {
+			if (index === 0) {
+				topPos += $(element).height();
+			} else {
+				let top = initialTop + topPos;
+
+				$(element).css({top: `${top}px`});
+
+				topPos += nextTop + $(element).height();
+			}
+		});
+	}
+
 	displayFlash() {
-		const flash = $('.RibsFlashMessage');
+		const flash = this.flash;
 
 		if (flash.length > 0) {
 			flash.hide().slideDown(500);
