f888fecd9763507da13e9b95060a1b959a1d7893
Anthony Pilloud <pilloud.anthony@gmail.com>
2017-12-16 12:12:04 +0100

    [func] change static method to dynamic method called in constructor

--- source/js/ribs-flash-message.js
+++ source/js/ribs-flash-message.js
@@ -1,26 +1,30 @@
 import $ from 'jQuery';
 
 class RibsFlashMessage {
-  static displayFlash() {
-    const flash = $('.RibsFlashMessage');
-    
-    if (flash.length > 0) {
-      flash.hide().slideDown(500);
-  
-      flash.click(function() {
-        $(this).slideUp();
-      });
-  
-      setTimeout(function() {
-          flash.slideUp();
-      }, 10000);
-  
-      setTimeout(function() {
-          flash.remove();
-      }, 12000);
-    }
-  }
+	constructor() {
+		this.displayFlash();
+	}
+
+	displayFlash() {
+		const flash = $('.RibsFlashMessage');
+
+		if (flash.length > 0) {
+			flash.hide().slideDown(500);
+
+			flash.click(function () {
+				$(this).slideUp();
+			});
+
+			setTimeout(function () {
+				flash.slideUp();
+			}, 10000);
+
+			setTimeout(function () {
+				flash.remove();
+			}, 12000);
+		}
+	}
 }
 export default (RibsFlashMessage);
 
-RibsFlashMessage.displayFlash();
+const ribsFlashMessage = new RibsFlashMessage();
