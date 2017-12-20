f58249e57b621f33afc2e03bb7947087c6f87448
piou-piou <pilloud.anthony@gmail.com>
2017-12-20 08:39:45 +0100

    [func] change indent

--- source/js/ribs-flash-message.js
+++ source/js/ribs-flash-message.js
@@ -7,7 +7,7 @@ class RibsFlashMessage {
 			this.displayFlash();
 		}
 	}
-  
+
   /**
    * @param element
    * @returns {Number}
@@ -17,7 +17,7 @@ class RibsFlashMessage {
 	  element.style.display = 'block';
 	  let height = parseInt(window.getComputedStyle(element).getPropertyValue('height'));
     element.style.display = '';
-    
+
     return height;
   }
 
@@ -27,20 +27,20 @@ class RibsFlashMessage {
 	setFlashPosition() {
 		const initialTop = 20;
 		let topPos = parseInt(window.getComputedStyle(this.flash[0]).getPropertyValue('top'));
-    
+
     Array.from(this.flash).forEach((element, index) => {
 			if (index === 0) {
 				topPos += this.getHeight(element);
 			} else {
 				let top = initialTop + topPos;
-				
+
 				element.style.top = `${top}px`;
 
 				topPos += initialTop + this.getHeight(element);;
 			}
 		});
 	}
-  
+
   /**
    * @param element
    * @param duration
@@ -49,34 +49,34 @@ class RibsFlashMessage {
    */
 	toggleSlide(element, duration) {
     let maxHeight = 0;
-	  
+
 	  if (window.getComputedStyle(element).getPropertyValue('max-height') === 'none') {
       maxHeight = this.getHeight(element);
     }
-    
+
     element.style.transition = 'max-height 0.5s ease-in-out';
     element.style.maxHeight = 0;
     element.style.display = 'block';
-    
+
     setTimeout(function() {
       element.style.maxHeight = `${maxHeight}px`;
     }, duration)
   }
-  
+
 
 	/**
 	 * method to display all flash message
 	 */
 	displayFlash() {
 	  const self = this;
-	  
+
     Array.from(this.flash).forEach((element, index) => {
       this.toggleSlide(element, 500);
-  
+
       setTimeout(function () {
         self.toggleSlide(element, 500);
       }, 10000);
-  
+
       setTimeout(function () {
         element.remove();
       }, 12000);
