753c6a666df963fe8bfc823e3e3669d82e137d0d
Anthony Pilloud <pilloud.anthony@gmail.com>
2018-07-14 13:17:24 +0200

    [edit] webpack and package to run on latest versions

--- webpack.config.js
+++ webpack.config.js
@@ -3,14 +3,14 @@ const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 const extractSass = new ExtractTextPlugin({
-  filename: "./dist/css/style.css",
+  filename: "./css/style.css",
   disable: process.env.NODE_ENV === "development"
 });
 
 module.exports = {
   entry: ['./source/scss/style.scss', './source/js/ribs-flash-message.js'],
   output: {
-    filename: 'dist/js/ribs-flash-message.js'
+    filename: 'js/ribs-flash-message.js'
   },
   module: {
     rules: [
