9bdae6cec494740e1779438918cfea095f33b15d
piou-piou <pilloud.anthony@gmail.com>
2018-07-17 13:15:46 +0200

    [edit] font-awesome loading now in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,22 @@ module.exports = {
           ],
           fallback: "style-loader"
         })
-      }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'fonts/',    // where the fonts will go
+            publicPath: '../fonts/'       // override the default path
+          }
+        }]
+      },
     ]
   },
   plugins: [
